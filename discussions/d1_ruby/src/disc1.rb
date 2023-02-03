c9e5d526619fdb7d29f8c70caff5c0304f597b7c
ichen2467 <83405884+ichen2467@users.noreply.github.com>
2023-02-03 12:45:04 -0500

    Update disc1.rb

--- discussions/d1_ruby/src/disc1.rb
+++ discussions/d1_ruby/src/disc1.rb
@@ -1,15 +1,13 @@
 def sum()
     a = [1,2,3,4,5]
     sum = 0
-    # insert code block here
-
+    a.each{|x| sum+=x}
     puts sum
 end
 
 def multiply()
     a = [1,2,3,4,5]
-    # insert code block here
-
+    a.collect{|x| x = x * 2}
     puts a.join(',')
 end
 
@@ -18,13 +16,19 @@ def population()
     countries["USA"] = 319 
     countries["Italy"] = 60
 
-    # insert code block here
+    countries{|k, v|
+        puts "population of #{k} is #{v} million"
+    }
 end
 
 def splitString(s)
-    # insert code here
+    for word in s.split(',')
+        yield word
+    end
 end
 
-# call splitString here (param: "welcome,to,ruby,discussion") with code block
+distance = Proc.new{|x_1, y_1, x_2, y_2| puts Math.sqrt((x_1).abs)}
+
+splitString("welcome to ruby discussion"){|x| puts x}
 
 
